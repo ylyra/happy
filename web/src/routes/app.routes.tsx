3f69941d2644240b775acad5c7701efc65b8c97b
Yan Lyra <yanlyra3@gmail.com>
2021-03-20 19:48:18 -0300

    :sparkles: projeto atualizado

--- web/src/routes/app.routes.tsx
+++ web/src/routes/app.routes.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Landing from './pages/Landing'
-import OrphanagesMap from './pages/OrphanagesMap'
+import Landing from '../pages/Landing'
+import OrphanagesMap from '../pages/OrphanagesMap'
 
 // Orphanage folder
-import Orphanage from './pages/orphanage/Orphanage'
-import Success from './pages/orphanage/Success'
-import CreateOrphanage from './pages/orphanage/CreateOrphanage'
+import Orphanage from '../pages/orphanage/Orphanage'
+import CreateOrphanage from '../pages/orphanage/CreateOrphanage'
+import EditOrphanage from '../pages/orphanage/EditOrphanage'
+import Success from '../pages/orphanage/Success'
 
-function Routes() {
+const AppRoute: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
+        <Route path="/admin" component={OrphanagesMap} />
 
         {/*Orphanage*/}
         <Route path="/orphanages/create" component={CreateOrphanage} />
         <Route path="/orphanages/:id" component={Orphanage} />
+
+        <Route path="/orphanages/edit/:id" component={EditOrphanage} />
         <Route path="/success" component={Success} />
       </Switch>
     </BrowserRouter>
   )
 }
 
-export default Routes;
+export default AppRoute;
--- web/src/routes/auth.routes.tsx
+++ web/src/routes/auth.routes.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Landing from '../pages/Landing'
+import OrphanagesMap from '../pages/OrphanagesMap'
+
+// Orphanage folder
+import Orphanage from '../pages/orphanage/Orphanage'
+
+
 // Dashboard folder
 import DashboardLogin from '../pages/dashboard/Login'
 import DashboardForgetPassword from '../pages/dashboard/ForgetPassword'
 import DashboardReset from '../pages/dashboard/Reset'
+import CreateOrphanage from '../pages/orphanage/CreateOrphanage';
 
 const AuthRoute: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>        
-        <Route path="/" exact component={DashboardLogin} />
+        <Route path="/" exact component={Landing} />
+        <Route path="/app" component={OrphanagesMap} />
+        <Route path="/admin" component={DashboardLogin} />
+
+        {/*Orphanage*/}
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
+
         <Route path="/login" component={DashboardLogin} />
         <Route path="/esqueci" component={DashboardForgetPassword} />
         <Route path="/resetar/" component={DashboardReset} />
